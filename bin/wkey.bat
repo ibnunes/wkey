892ac4541f576c506d29ab6b41325a50773346f4
thoga31 <thoga31@gmail.com>
2015-06-21 07:01:30 +0100

    Small correction of comments

--- bin/wkey.bat
+++ bin/wkey.bat
@@ -1,8 +1,8 @@
 @echo off
 
 REM  ===== WKey =====
-REM  Ver: 1.0.0
-REM    By: Igor Nunes
+REM  Ver: 1.0
+REM    By: thoga31
 REM  Date: June 21st, 2015
 REM  Windows Key Decoder - for when your label wore out or doesn't even exist (e.g., laptops with Windows 8/8.1).
 REM  Must be used only for genuine Windows when you need your original key to reinstall the OS.
